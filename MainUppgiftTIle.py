f41d7a0bea201089ae7d510204f3e22fee2475f5
AlfonsDahlberg <125504824+AlfonsDahlberg@users.noreply.github.com>
2023-04-10 13:54:55 +0200

    Uppdatering: Fungerande spel V1

--- MainUppgiftTIle.py
+++ MainUppgiftTIle.py
@@ -1,6 +1,31 @@
+#importerar nödvändiga moduler och all nödvändig kod.
 import pygame, sys
-from settings import all *
+from inställningar import *
+from LEVEL import Level
 
 #pygame setup
 pygame.init()
-screen = pygame.display.set_mode((SKÄRM))
+screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+pygame.display.set_caption("PythonSpel")
+
+LEVEL = Level()
+
+#loop för händelser.
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        #lägger till en "quit" shortcut på "ESC"
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            pygame.quit()
+            sys.exit()
+    screen.blit(BG_COLOR, (0, 0))
+    LEVEL.run()
+
+    # ritar logic
+    pygame.display.update()
+    clock.tick(60)
+
+
--- inställningar.py
+++ inställningar.py
@@ -1,3 +1,30 @@
+import pygame
+LEVEL_MAP = [
+'                            ',
+'                            ',
+'                            ',
+'       XXXX           XX    ',
+'   P                        ',
+'XXXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
 TILE_SIZE = 64
-SCREEN_WIDTH =1280
-SCREEN
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+# colors
+BG_COLOR = pygame.image.load("bgpic.png")
+PLAYER_COLOR = pygame.image.load("spelaren.png")
+TILE_COLOR = pygame.image.load("bricktile.png")
+
+# camera
+CAMERA_BORDERS = {
+	'left': 100,
+	'right': 200,
+	'top':100,
+	'bottom': 150
+}
